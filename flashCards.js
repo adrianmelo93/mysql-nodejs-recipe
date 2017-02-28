021902a02d02befcd4f92f76649ef2a233a65629
Adrian Melo <amelo2@fvi.edu>
2017-02-28 18:22:03 -0500

    finish flashcards

--- flashCards.js
+++ flashCards.js
@@ -8,7 +8,7 @@ module.exports = function(app, connection){
     res.json(rows);
   });
   });
-  
+
   app.post('/flashcards', function(req, res){
   var query = `insert INTO flashcards (front_text, back_text, owner, subject)
         VALUES('${req.body.front_text}', '${req.body.back_text}', '${req.body.owner}', '${req.body.subject}')`;
@@ -19,6 +19,18 @@ console.log(query);
       return res.sendStatus(500);
     }
   res.json(result);
+  });
+});
+app.put('/flashcards', function(req, res){
+  var q = `UPDATE flashcards set front_text= '${req.body.front_text}', back_text='${req.body.back_text}', subject='${req.body.subject}'
+  where id=${req.body.id}`;
+  connection.query(q, function(err, result){
+    if(err){
+      console.log("Error updating flashcards with id: "+req.body.id);
+      console.log(err.toString());
+      return res.sendStatus(500);
+    }
+    res.json(result);
   });
 });
 }
